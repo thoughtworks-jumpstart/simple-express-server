1d3ccc6cd9b90da9570d46f98f0304103626306c
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-10-06 18:03:00 +0800

    Log Node.js environment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const app = require('./app')
-const port = 3000
+const app = require("./app");
+const env = app.get("env"); // process.env.NODE_ENV
+const port = env === "development" ? 3000 : 5000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`Listening on port ${port} in ${env} mode`);
+});
